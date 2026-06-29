6e03f8e690c37d1eecd2f1f3a55f70cc72a11a26
moltencrux <moltencrux@gmail.com>
2026-06-29 23:56:40 +0800

    feat: add english & simp. Chinese translations
    - Change base language to English from traditional Chinese
    - Add translation to simplified Chinese
    - Update .gitignore to ignore compiled .mo files

--- config/urls.py
+++ config/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.i18n import i18n_patterns
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("django.contrib.auth.urls")),
     path("accounts/", include("apps.accounts.urls")),
     path("", include("apps.recommender.urls")),
+    path('i18n/', include('django.conf.urls.i18n')),
 ]
+
