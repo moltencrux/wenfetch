6d4c3e7421ebabe92bbb1de979d2c4c25d605dbe
moltencrux <moltencrux@gmail.com>
2026-06-29 23:56:40 +0800

    feat: add English & simp. Chinese translations
    - Change base language to English from traditional Chinese
    - Add translation to simplified Chinese
    - Update .gitignore to ignore compiled .mo files

--- config/settings/base.py
+++ config/settings/base.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from django.utils.translation import gettext_lazy as _
 
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
@@ -19,6 +20,7 @@ MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
+    "django.middleware.locale.LocaleMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
@@ -59,7 +61,15 @@ AUTH_PASSWORD_VALIDATORS = [
     {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
 ]
 
-LANGUAGE_CODE = "en-us"
+LANGUAGES = [
+    ('en', _('English')),
+    ('zh-hant', _('繁體中文')),  # or 'zh-tw'
+    ('zh-hans', _('简体中文')),  # or 'zh-cn'
+]
+
+LOCALE_PATHS = [BASE_DIR / 'locale']  # Where .po files will live
+
+LANGUAGE_CODE = "en"
 TIME_ZONE = "Asia/Taipei"
 USE_I18N = True
 USE_TZ = True
